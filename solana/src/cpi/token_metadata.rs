e07cc6a76d4eba2a4aa270e55cbf556955543473
Joe C <joe.caulfield@solana.org>
2023-05-01 02:22:47 -0500

    more metadata (#40)

--- solana/src/cpi/token_metadata.rs
+++ solana/src/cpi/token_metadata.rs
@@ -4,8 +4,9 @@ use solana_program::{
     account_info::AccountInfo, entrypoint::ProgramResult, program::invoke, pubkey::Pubkey,
 };
 
-use crate::{Create, Metadata, NautilusAccountInfo, NautilusSigner};
+use crate::{Create, Metadata, NautilusAccountInfo, NautilusMut, NautilusSigner};
 
+/// Creates a Metadata account with the Token Metadata Program.
 #[allow(clippy::boxed_local)]
 #[allow(clippy::too_many_arguments)]
 pub fn create_metadata_v3<'a>(
@@ -44,6 +45,94 @@ pub fn create_metadata_v3<'a>(
             *mint.account_info(),
             *mint_authority.account_info(),
             *payer.account_info(),
+            *rent,
+        ],
+    )
+}
+
+/// Creates a MasterEdition account with the Token Metadata Program.
+#[allow(clippy::boxed_local)]
+#[allow(clippy::too_many_arguments)]
+pub fn create_master_edition_v3<'a>(
+    token_metadata_program_id: &Pubkey,
+    edition: impl NautilusMut<'a>,
+    mint: impl NautilusAccountInfo<'a>,
+    metadata: impl NautilusAccountInfo<'a>,
+    update_authority: impl NautilusSigner<'a>,
+    mint_authority: impl NautilusSigner<'a>,
+    payer: impl NautilusSigner<'a>,
+    rent: Box<AccountInfo<'a>>,
+    max_supply: Option<u64>,
+) -> ProgramResult {
+    invoke(
+        &mpl_token_metadata::instruction::create_master_edition_v3(
+            *token_metadata_program_id,
+            *edition.key(),
+            *mint.key(),
+            *update_authority.key(),
+            *mint_authority.key(),
+            *metadata.key(),
+            *payer.key(),
+            max_supply,
+        ),
+        &[
+            *edition.account_info(),
+            *metadata.account_info(),
+            *mint.account_info(),
+            *mint_authority.account_info(),
+            *payer.account_info(),
+            *rent,
+        ],
+    )
+}
+
+/// Mints a new Edition from a MasterEdition.
+#[allow(clippy::boxed_local)]
+#[allow(clippy::too_many_arguments)]
+pub fn mint_edition_from_master_edition<'a>(
+    token_metadata_program_id: &Pubkey,
+    mint: impl NautilusMut<'a>,
+    metadata: impl NautilusAccountInfo<'a>,
+    edition: impl NautilusMut<'a>,
+    master_edition: impl NautilusAccountInfo<'a>,
+    master_edition_mint: impl NautilusAccountInfo<'a>,
+    master_edition_metadata: impl NautilusAccountInfo<'a>,
+    to: impl NautilusMut<'a>,
+    to_authority: impl NautilusSigner<'a>,
+    mint_authority: impl NautilusSigner<'a>,
+    update_authority: impl NautilusSigner<'a>,
+    payer: impl NautilusSigner<'a>,
+    rent: Box<AccountInfo<'a>>,
+    edition_val: u64,
+) -> ProgramResult {
+    invoke(
+        &mpl_token_metadata::instruction::mint_new_edition_from_master_edition_via_token(
+            *token_metadata_program_id,
+            *metadata.key(),
+            *edition.key(),
+            *master_edition.key(),
+            *mint.key(),
+            *mint_authority.key(),
+            *payer.key(),
+            *to_authority.key(),
+            *to.key(),
+            *update_authority.key(),
+            *master_edition_metadata.key(),
+            *master_edition_mint.key(),
+            edition_val,
+        ),
+        &[
+            *metadata.account_info(),
+            *edition.account_info(),
+            *master_edition.account_info(),
+            *mint.account_info(),
+            *mint_authority.account_info(),
+            *payer.account_info(),
+            *to_authority.account_info(),
+            *to.account_info(),
+            *update_authority.account_info(),
+            *master_edition_metadata.account_info(),
+            *master_edition_mint.account_info(),
             *rent,
         ],
     )
